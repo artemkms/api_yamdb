cd142daf095e521660b8d4427505f1792fe2ff70
viator3m <viator3m@gmail.com>
2022-06-21 19:16:57 +0300

    Added custom User model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.is_superuser:
+            self.role = 'admin'
+
+    ROLES = (
+        ('user', 'Пользователь'),
+        ('moderator', 'Модератор'),
+        ('admin', 'Админ'),
+    )
+
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        unique=True,
+        max_length=250,
+    )
+    bio = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+        max_length=250,
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        default='user',
+        choices=ROLES,
+        max_length=16,
+    )
+    confirmation_code = models.CharField(
+        verbose_name='Код подтверждения',
+        blank=True,
+        max_length=50
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('username', 'email'),
+                name='unique_user'
+            )
+        ]
