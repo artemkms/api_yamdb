ec2cb8f1926658dfb7dcf242865e27dd01247c77
Павел Гавриленко <train.000@yandex.ru>
2022-06-21 20:19:50 +0300

    Обьекты и регистрация в админке

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Title, Category, Genre, GenreTitle
+
+admin.site.register(Title)
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(GenreTitle)
