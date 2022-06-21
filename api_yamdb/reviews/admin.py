ed6e625bc24f917273da066135e46be64a819e6f
viator3m <viator3m@gmail.com>
2022-06-21 20:49:30 +0300

    Custom fields in admin-zone.

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from models import User
+from .models import User
 
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'role', 'email', 'first_name',
+                    'last_name', 'is_staff', 'is_superuser')
+
+
+admin.site.register(User, CustomUserAdmin)
