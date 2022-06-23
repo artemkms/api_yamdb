59cedbff2124ef989b2a4752bdb08ba2d6eb000e
viator3m <viator3m@gmail.com>
2022-06-23 13:04:05 +0300

    Validate username (must not use 'me')

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -7,3 +7,10 @@ class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError(
+                'Невозможно создать пользователя с таким именем'
+            )
+        return value
