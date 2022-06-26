2ae5add092a432b6d25714c30c153642a8271e46
Павел Гавриленко <train.000@yandex.ru>
2022-06-26 19:09:33 +0300

    правки проходящие тесты

---
+++ api_yamdb/api/filters.py
@@ -0,0 +1,12 @@
+from django_filters import filters
+from django_filters.rest_framework import FilterSet
+from reviews.models import Title
+
+
+class TitleFilter(FilterSet):
+    genre = filters.CharFilter(field_name='genre__slug')
+    category = filters.CharFilter(field_name='category__slug')
+    name = filters.CharFilter(field_name='name', lookup_expr='contains')
+    class Meta:
+        model = Title
+        fields = '__all__'
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,13 +1,15 @@
 from rest_framework import mixins
 from rest_framework import viewsets
 from rest_framework import status
-
 from rest_framework.response import Response
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from reviews.models import Category, Genre, Title
 from api.serializers import CategorySerializer, GenresSerializer
 from api.serializers import TitlesSerializer, TitlesPOSTSerializer
 from api.permissions import ReadOnly, IsRoleAdmin
+from api.filters import TitleFilter
 
 
 class ListPostDel(
@@ -22,6 +24,9 @@ class CategoryViewSet(ListPostDel):
     serializer_class = CategorySerializer
     permission_classes = (IsRoleAdmin | ReadOnly,)
     lookup_field = 'slug'
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_fields = ('name',)
+    search_fields = ('name',)
 
 
 class GenreViewSet(ListPostDel):
@@ -29,12 +34,17 @@ class GenreViewSet(ListPostDel):
     serializer_class = GenresSerializer
     permission_classes = (IsRoleAdmin | ReadOnly,)
     lookup_field = 'slug'
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_fields = ('name',)
+    search_fields = ('name',)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitlesSerializer
     permission_classes = (IsRoleAdmin | ReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
 
     def get_serializer_class(self):
         if self.request.method in ('POST', 'PATCH',):
@@ -53,3 +63,4 @@ class TitleViewSet(viewsets.ModelViewSet):
         super().update(request, *args, **kwargs)
         out_objecy = TitlesSerializer(instance=self.get_object())
         return Response(out_objecy.data)
+
