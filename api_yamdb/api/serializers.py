9a0cf44066dd46b09a11a3886d866661a0d88f2f
artemkms <69520287+artemkms@users.noreply.github.com>
2022-06-27 00:19:18 +0700

    next step

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,53 @@
+import datetime as dt
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from reviews.models import (
+    Category,
+    Comment,
+    Genre,
+    Review,
+    Title,
+)
+from users.models import User
+
+class ReviewsSerializer(serializers.ModelSerializer):
+    """Ревью для произведений"""
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        default=serializers.CurrentUserDefault(),
+        read_only=True
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+        read_only_fields = ['title']
+
+    def validate(self, data):
+        if self.context['request'].method == 'POST':
+            title_id = (
+                self.context['request'].parser_context['kwargs']['title_id']
+            )
+            user = self.context['request'].user
+            if user.reviews.filter(title_id=title_id).exists():
+                raise serializers.ValidationError(
+                    'Нельзя оставить отзыв на одно произведение дважды'
+                )
+        return data
+
+    def validate_score(self, value):
+        if 0 >= value >= 10:
+            raise serializers.ValidationError('Проверьте оценку')
+        return value
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    """Комментарии на отзывы"""
+
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'pub_date')
+        model = Comment
+
