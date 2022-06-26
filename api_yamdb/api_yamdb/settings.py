1a139c36c056285bbda6e13b500057452f366fe1
Павел Гавриленко <train.000@yandex.ru>
2022-06-26 16:13:39 +0300

    Восстановленая версия после пробел пула

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,8 +22,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'reviews.apps.ReviewsConfig',
     'api.apps.ApiConfig',
+
 ]
 
 MIDDLEWARE = [
